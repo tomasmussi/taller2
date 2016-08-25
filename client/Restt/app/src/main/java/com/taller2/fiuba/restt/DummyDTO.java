87ce4c0722a73d2a659b2ec91f8d6f954f3f9f72
Luis Arancibia <Luis Arancibia>
2016-08-25 16:55:30 -0300

    agregado un prototipo de la app client

---
+++ client/Restt/app/src/main/java/com/taller2/fiuba/restt/DummyDTO.java
@@ -0,0 +1,25 @@
+package com.taller2.fiuba.restt;
+
+/**
+ * Created by aranc on 21/8/2016.
+ */
+public class DummyDTO {
+    public String id;
+    public String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
