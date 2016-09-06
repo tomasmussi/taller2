77e665e5b4b40f465a852f2529d14b49cac9bb9b
tomas <tomasmussi@gmail.com>
2016-09-06 18:03:19 -0300

    Instalacion de biblioteca de leveldb

--- travis.sh
+++ travis.sh
@@ -1,8 +1,19 @@
-#!/bin/bash          
+#!/bin/bash
 
+# Instalacion biblioteca jsoncpp
 git clone https://github.com/open-source-parsers/jsoncpp.git
 cd jsoncpp
 mkdir build
 cd build
 cmake ..
 sudo make install
+
+# Instalacion biblioteca leveldb
+cd ../..
+git clone https://github.com/google/leveldb.git
+cd leveldb
+make
+cd out-shared sudo cp --preserve=links libleveldb.* /usr/local/lib
+cd ../include
+sudo cp -R leveldb /usr/local/include/
+sudo ldconfig
