4808cdd403480e9a9ebeef5d450ba55120ad078a
tomas <tomasmussi@gmail.com>
2016-09-09 15:14:39 -0300

    Pruebo una prueba que falla para ver travis

--- server/test/test.cpp
+++ server/test/test.cpp
@@ -6,9 +6,14 @@ TEST(DBTEST, WriteAndRead) {
 	DBHandler db("prueba");
 	std::string padron("91985");
 	db.write("padron_eze","91985");
-	std::string result = db.read("padron_eze");    
+	std::string result = db.read("padron_eze");
 	EXPECT_EQ(result,padron);
 }
+
+TEST(Gtest, Test1Equals0) {
+	EXPECT_EQ(1,0);
+}
+
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest( &argc, argv );
