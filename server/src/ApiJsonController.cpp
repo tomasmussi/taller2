dd2c5d00da3af53ad2fd55f319cb798e02cc00f8
tomas <tomasmussi@gmail.com>
2016-09-21 16:35:18 -0300

    Agrego proyecto curlpp

--- server/src/ApiJsonController.cpp
+++ server/src/ApiJsonController.cpp
@@ -1,5 +1,9 @@
 #include "ApiJsonController.h"
 
+#include <curlpp/cURLpp.hpp>
+#include <curlpp/Easy.hpp>
+#include <curlpp/Options.hpp>
+
 ApiJsonController::ApiJsonController(DBHandler *database_handler) : database_handler_(database_handler) {
 }
 
@@ -19,11 +23,38 @@ void ApiJsonController::setup() {
 
 
 void ApiJsonController::hello(Mongoose::Request &request, Mongoose::JsonResponse &response) {
+	/*
 	int i;
 	for (i=0; i<12; i++) {
 		response["users"][i]["Name"] = "Bob";
 	}
 	response["timestamp"] = (int)time(NULL);
+	*/
+	try
+	{
+		// That's all that is needed to do cleanup of used resources (RAII style).
+		curlpp::Cleanup myCleanup;
+
+		// Our request to be sent.
+		curlpp::Easy myRequest;
+
+		// Set the URL.
+		myRequest.setOpt<Url>("http://example.com");
+
+		// Send request and get a result.
+		// By default the result goes to standard output.
+		myRequest.perform();
+	}
+
+	catch(curlpp::RuntimeError & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	catch(curlpp::LogicError & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 }
 
 void ApiJsonController::testdb(Mongoose::Request &request, Mongoose::JsonResponse &response) {
