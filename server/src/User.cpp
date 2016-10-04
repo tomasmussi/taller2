587f0d1f54086e5004371235b4e5c239ba7780e6
tomas <tomasmussi@gmail.com>
2016-10-04 16:26:53 -0300

    First test passing

--- server/src/User.cpp
+++ server/src/User.cpp
@@ -1,14 +1,35 @@
 #include "User.h"
 
 #include <json/json.h>
+#include <string>
 
 User::User() {
 }
 
+/* Construct User from string parsed as JSON
+ * JSON format is:
+ * {
+   "user":{
+      "name":"Tomas Mussi",
+      "email":"tomasmussi@gmail.com",
+      "pass":"tomas",
+      "dob":"11/07/1991",
+      "city":"Ciudad de Buenos Aires",
+      "summary":"Estudiante de ingenieria informatica de la UBA.",
+      "skills":[
+         1,
+         2
+      ],
+      "contacts":4,
+      "profile_photo":"QURQIEdtYkgK...dHVuZw=="
+   }
+   }
+ */
 User::User(std::string json_value) {
 	Json::Value root;
 	Json::Reader reader;
 	reader.parse(json_value, root);
+	email_ = root["user"]["email"].asString();
 }
 
 User::~User() {
