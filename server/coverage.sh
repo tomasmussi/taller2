97c042e16f5ae212abd8fb5bad4bbce556860efe
Eze <eze1214@live.com.ar>
2016-10-21 03:10:14 -0300

    Agregado un script que ejecuta los test, y agregado al gitignore para que no se comitee la salida del codecoverage

---
+++ server/coverage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+if [ -d ./build/test ] 
+then
+./build/test/testing
+lcov --capture --directory ./build/test/CMakeFiles/testing.dir/__/src/ --output-file ./build/test/CMakeFiles/testing.dir/__/src/coverage.info
+genhtml ./build/test/CMakeFiles/testing.dir/__/src/coverage.info --output-directory ./coverage
+firefox ./coverage/src/index.html;
+else
+echo "Los test no está compilados"
+fi
+exit
