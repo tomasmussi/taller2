d2e5766ba5693acc259be5dccb65aad5d9886f03
alfredo146 <ackermannal@gmail.com>
2016-10-29 22:53:52 -0300

    completando alta de job position

--- SharedServerHeroku/views/pages/js/jobPosition/jobPositionController.js
+++ SharedServerHeroku/views/pages/js/jobPosition/jobPositionController.js
@@ -1,4 +1,4 @@
-mainApp.controller('jobPositionController', function($scope,$http) {
+mainApp.controller('jobPositionController', function($scope,$http,$window) {
 	
     $scope.jobPosition = {
       name: "Ej: developer",
@@ -8,6 +8,8 @@ mainApp.controller('jobPositionController', function($scope,$http) {
 			$scope.jobPosition.name = "Ej: developer";
 			$scope.jobPosition.description = "Ej: desarrollador web";
 			$scope.jobPosition.category = "Ej: software";
+            $scope.jobPosition.error = "";
+            $scope.jobPosition.errorState=false;
 		},
 		submit : function(){
 			var data = {
@@ -17,15 +19,15 @@ mainApp.controller('jobPositionController', function($scope,$http) {
             $http.post('http://localhost:5000/job_positions/categories/'+$scope.jobPosition.category, data)
             .success(function (data, status, headers, config) {
                 $scope.PostDataResponse = data;
+                $window.location.href = 'http://localhost:5000';
             })
             .error(function (data, status, header, config) {
-                $scope.ResponseDetails = "Data: " + data +
-                    "<hr />status: " + status +
-                    "<hr />headers: " + header 
+                $scope.jobPosition.error="Error al guardar Job Position, por favor presione Reset y vuelva a intentar";
+                $scope.jobPosition.errorState=true;
             });
 		}
 
 		
 	}
-    
+
 });
