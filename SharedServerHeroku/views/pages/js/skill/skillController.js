fb76e76c0a4fde7a4cfba3f250a0ea9d55bc7ab4
alfredo146 <ackermannal@gmail.com>
2016-11-12 22:11:03 -0300

    Alta,baja y modificacion de skills

--- SharedServerHeroku/views/pages/js/skill/skillController.js
+++ SharedServerHeroku/views/pages/js/skill/skillController.js
@@ -16,13 +16,13 @@ mainApp.controller('skillController', function($scope,$http,$window, $route, $ro
                 "name": $scope.skill.name,
                 "description": $scope.skill.description
             };
-            $http.post('http://localhost:5000/skill/categories/'+$scope.skill.category, data)
+            $http.post('http://localhost:5000/skills/categories/'+$scope.skill.category, data)
             .success(function (data, status, headers, config) {
                 $scope.PostDataResponse = data;
                 $window.location.href = 'http://localhost:5000';
             })
             .error(function (data, status, header, config) {
-                $scope.skill.error="Error al guardar Job Position, por favor presione Reset y vuelva a intentar";
+                $scope.skill.error="Error al guardar skill, por favor presione Reset y vuelva a intentar";
                 $scope.skill.errorState=true;
             });
         },
@@ -32,13 +32,13 @@ mainApp.controller('skillController', function($scope,$http,$window, $route, $ro
                 "description": $scope.skill.description,
                 "category": $scope.skill.category
             };
-            $http.put('http://localhost:5000/skill/categories/'+$scope.categoryOld+'/'+$scope.nameOld, data)
+            $http.put('http://localhost:5000/skills/categories/'+$scope.categoryOld+'/'+$scope.nameOld, data)
             .success(function (data, status, headers, config) {
                 $scope.PostDataResponse = data;
                 $window.location.href = 'http://localhost:5000';
             })
             .error(function (data, status, header, config) {
-                $scope.skill.error="Error al guardar Job Position, por favor presione Reset y vuelva a intentar";
+                $scope.skill.error="Error al guardar skill, por favor presione Reset y vuelva a intentar";
                 $scope.skill.errorState=true;
             });
         },
@@ -50,13 +50,13 @@ mainApp.controller('skillController', function($scope,$http,$window, $route, $ro
                 "name":  $scope.nameToDelete,
                 "category": $scope.categoryToDelete
             };
-            $http.delete('http://localhost:5000/skill/categories/'+$scope.categoryToDelete+'/'+$scope.nameToDelete,data)
+            $http.delete('http://localhost:5000/skills/categories/'+$scope.categoryToDelete+'/'+$scope.nameToDelete,data)
             .success(function (data, status, headers, config) {
                 
                 $window.location.href = 'http://localhost:5000';
             })
             .error(function (data, status, header, config) {
-                $scope.skill.error="Error al Borra Job Position, por favor presione Cancelar y vuelva a intentar";
+                $scope.skill.error="Error al Borra skill, por favor presione Cancelar y vuelva a intentar";
                 $scope.skill.errorState=true;
             });
         }
