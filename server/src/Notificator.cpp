0b2ea05f2b696ee4f13f47c249bfa1cd5d6b31e7
ezequiel dufau <eze1214@live.com.ar>
2016-11-15 19:26:55 -0300

    Notificator terminado, se pushea la notificacion al server de FCM

--- server/src/Notificator.cpp
+++ server/src/Notificator.cpp
@@ -1,6 +1,10 @@
 #include "Notificator.h"
 #include "sstream"
 #include <json/json.h>
+#include <iostream>
+#include <curlpp/cURLpp.hpp>
+#include <curlpp/Easy.hpp>
+#include <curlpp/Options.hpp>
 
 Notificator::Notificator(Token_FCM & token,TYPENOTIFICATOR type, std::string message):
 	FIREBASE_KEY("AIzaSyB0Dd993SZP4dAUySqZzcNZnt4HFd_RLWo"){
@@ -11,7 +15,7 @@ Notificator::Notificator(Token_FCM & token,TYPENOTIFICATOR type, std::string mes
 }
 
 void Notificator::send(){
-	/*curlpp::options::Url myUrl("https://fcm.googleapis.com/fcm/send");
+	curlpp::options::Url myUrl("https://fcm.googleapis.com/fcm/send");
 	curlpp::Easy myRequest;
 	myRequest.setOpt(myUrl);
 
@@ -20,14 +24,14 @@ void Notificator::send(){
 	std::ostringstream Authorization;
 	Authorization<<"Authorization: key=";
 	Authorization<<FIREBASE_KEY;
-
+	std::cout<<Authorization.str()<<std::endl;
 	header.push_back(Authorization.str());
 	myRequest.setOpt(new curlpp::options::HttpHeader(header));
-*/	
-	Json::Value jsoni;
+	
+	Json::Value json;
 	Json::Value data;
  
-	data["message"] = this->message;
+	data["mensaje"] = this->message;
 	switch(type){
 		case TYPENOTIFICATOR::CHAT:
 			data["type"] = "CHAT";
@@ -40,13 +44,13 @@ void Notificator::send(){
 	}
 	data["fb_ib_sender"] = this->fb_id_sender;
 
-	Json::Value to;
-	to["to"] = this->tokenFCM;
+	json["data"] = data;
+	json["to"] = this->tokenFCM;
 
-	jsoni["data"].append(data);
-	jsoni["to"].append(to);
-	//std::string body = "{\"data\": { \"mensaje\": \"oh por dios\"}, \"to\": \"eDSpyrzlxKQ:APA91bGBze7mTQK3OnIWJf-WTNRIYvFDbLNGsVjtMMliVRcjUF6AqsNbZhXTYcSS5srb6fSUSZ-yrB9NC7mX2hV6AjJAmF1Vz2fFuWVUX8oSDnPV1KcnStt6DNR0gyhLibjrMXuu2-MA\"}";
-	/*myRequest.setOpt(new curlpp::options::PostFields(body.c_str()));
+	Json::FastWriter fastWriter;
+	std::string body = fastWriter.write(json);
+	std::cout<<body<<std::endl; 
+	myRequest.setOpt(new curlpp::options::PostFields(body.c_str()));
 	myRequest.setOpt(new curlpp::options::PostFieldSize(body.size()));
-	myRequest.perform();*/
+	myRequest.perform();
 }
