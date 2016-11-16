fa48b5aa214fb8bb1d8df8619f3ebc7c6707550a
alfredo146 <ackermannal@gmail.com>
2016-11-15 21:39:50 -0300

    fix some urls

--- SharedServerHeroku/views/pages/js/jobPosition/jobPositionController.js
+++ SharedServerHeroku/views/pages/js/jobPosition/jobPositionController.js
@@ -16,10 +16,10 @@ mainApp.controller('jobPositionController', function($scope,$http,$window, $rout
                 "name": $scope.jobPosition.name,
                 "description": $scope.jobPosition.description
             };
-            $http.post('https://localhost:5000/job_positions/categories/'+$scope.jobPosition.category, data)
+            $http.post('http://localhost:5000/job_positions/categories/'+$scope.jobPosition.category, data)
             .success(function (data, status, headers, config) {
                 $scope.PostDataResponse = data;
-                $window.location.href = 'https://localhost:5000';
+                $window.location.href = 'http://localhost:5000';
             })
             .error(function (data, status, header, config) {
                 $scope.jobPosition.error="Error al guardar Job Position, por favor presione Reset y vuelva a intentar";
@@ -32,10 +32,10 @@ mainApp.controller('jobPositionController', function($scope,$http,$window, $rout
                 "description": $scope.jobPosition.description,
                 "category": $scope.jobPosition.category
             };
-            $http.put('https://localhost:5000/job_positions/categories/'+$scope.categoryOld+'/'+$scope.nameOld, data)
+            $http.put('http://localhost:5000/job_positions/categories/'+$scope.categoryOld+'/'+$scope.nameOld, data)
             .success(function (data, status, headers, config) {
                 $scope.PostDataResponse = data;
-                $window.location.href = 'https://localhost:5000';
+                $window.location.href = 'http://localhost:5000';
             })
             .error(function (data, status, header, config) {
                 $scope.jobPosition.error="Error al guardar Job Position, por favor presione Reset y vuelva a intentar";
@@ -43,17 +43,17 @@ mainApp.controller('jobPositionController', function($scope,$http,$window, $rout
             });
         },
         cancel : function(){
-            $window.location.href = 'https://localhost:5000';
+            $window.location.href = 'http://localhost:5000';
         },
         delete : function(){
             var data = {
                 "name":  $scope.nameToDelete,
                 "category": $scope.categoryToDelete
             };
-            $http.delete('https://localhost:5000/job_positions/categories/'+$scope.categoryToDelete+'/'+$scope.nameToDelete,data)
+            $http.delete('http://localhost:5000/job_positions/categories/'+$scope.categoryToDelete+'/'+$scope.nameToDelete,data)
             .success(function (data, status, headers, config) {
                 
-                $window.location.href = 'https://localhost:5000';
+                $window.location.href = 'http://localhost:5000';
             })
             .error(function (data, status, header, config) {
                 $scope.jobPosition.error="Error al Borra Job Position, por favor presione Cancelar y vuelva a intentar";
