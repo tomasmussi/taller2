742fbe99e88cb71918ea4302bd99902cb04ad65b
ezequiel dufau <eze1214@live.com.ar>
2016-11-17 11:41:26 -0300

    Agregado en el script coverage.sh el test del server

--- server/coverage.sh
+++ server/coverage.sh
@@ -2,7 +2,7 @@
 # -*- ENCODING: UTF-8 -*-
 if [ -d ./build/test ] 
 then
-./build/test/testing
+./build/test/testing 1
 lcov --capture --directory ./build/test/CMakeFiles/testing.dir/__/src/ --output-file ./build/test/CMakeFiles/testing.dir/__/src/coverage.info
 genhtml ./build/test/CMakeFiles/testing.dir/__/src/coverage.info --output-directory ./coverage
 firefox ./coverage/src/index.html;
